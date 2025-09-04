83a4a694884a3eec457858e1e58197f4f17867e7
Jared Briggs <1jaredbriggs@gmail.com>
2025-09-04 09:23:39 -0600

    search commit

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,30 @@
+import { openai } from '@ai-sdk/openai';
+import { streamText, UIMessage, convertToModelMessages, tool } from 'ai';
+import { z } from 'zod';
+
+export const maxDuration = 30;
+
+export async function GET() {
+    return new Response('chat+search route alive', {status: 200});
+}
+
+export async function POST(req: Request) {
+    const { messages }: { messages: UIMessage[] } = await req.json();
+
+    const result = streamText({
+        model: openai('gpt-4o-mini'),
+        messages: convertToModelMessages(messages),
+
+        // Enable OpenAI's web search tool
+        tools: {
+            web_search_preview: openai.tools.webSearchPreview({
+
+
+            }),
+        }
+
+
+    });
+    
+   return result.toUIMessageStreamResponse(); 
+}
